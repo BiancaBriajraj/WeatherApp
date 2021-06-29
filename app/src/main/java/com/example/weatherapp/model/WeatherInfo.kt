9114d1b14adbe27160722c4ff2803a0495f0aa7a
Bianca <biancabriajraj@gmail.com>
2021-06-29 13:38:49 +0200

    First Fragment working

--- app/src/main/java/com/example/weatherapp/model/WeatherInfo.kt
+++ app/src/main/java/com/example/weatherapp/model/WeatherInfo.kt
@@ -1,7 +1,10 @@
 package com.example.weatherapp.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class WeatherInfo(
     val coord: Coord,
     val weather: ArrayList<Weather>,
@@ -16,21 +19,21 @@ data class WeatherInfo(
     val id: Int,
     val name: String,
     val cod: Int,
-    val message: String)
+    val message: String) :Parcelable
 
-
+@Parcelize
 data class Coord(
     val lon: Double,
-    val lat: Double )
+    val lat: Double ) :Parcelable
 
-
+@Parcelize
 data class Weather(
     val id: Int,
     val main: String,
     val description:String,
-    val icon:String )
+    val icon:String ) :Parcelable
 
-
+@Parcelize
 data class Main(
     val temp: Double,
     @SerializedName("feels_like")
@@ -40,25 +43,25 @@ data class Main(
     @SerializedName("temp_max")
     val tempMax: Double,
     val pressure: Int,
-    val humidity: Int )
+    val humidity: Int ) :Parcelable
 
-
+@Parcelize
 data class Wind(
     val speed: Double,
     val deg: Int,
-    val gust: Double)
+    val gust: Double) :Parcelable
 
-
+@Parcelize
 data class Clouds(
-    val all: Int )
+    val all: Int ):Parcelable
 
-
+@Parcelize
 data class Sys(
     val type: Int,
     val id: Int,
     val country: String,
     val sunrise: Int,
-    val sunset: Int)
+    val sunset: Int):Parcelable
 
 
 
--- app/src/main/java/com/example/weatherapp/view/ListFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/ListFragment.kt
@@ -1,61 +1,80 @@
 package com.example.weatherapp.view
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentListBinding
 import com.example.weatherapp.model.CustomCallback
 import com.example.weatherapp.model.WeatherInfo
 import com.example.weatherapp.viewModel.WeatherViewModel
+import kotlinx.android.synthetic.main.fragment_list.*
 
 
 class ListFragment : Fragment() {
 
-    private  lateinit var binding: FragmentListBinding
+    private lateinit var binding: FragmentListBinding
 
     private lateinit var viewModel: WeatherViewModel
 
-      override  fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View {
-            // Inflate the layout for this fragment
-            binding = FragmentListBinding.inflate(inflater, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentListBinding.inflate(inflater, container, false)
 
-            return binding.root
-        }
-
-        @SuppressLint("WrongConstant")
-       override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-
-            viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
-
-            binding.searchButton.setOnClickListener {
-                val userInput = binding.searchText.text.toString()
-                viewModel.getInformation(userInput,object : CustomCallback {
-                         override fun onSuccess(value: WeatherInfo) {
-                                val v1=  value
-                                binding.testingId.text = value.name
-                           }
-
-                        override fun onFailure(errorCode:Int ,message: String) {
-                                binding.testingId.text = message
-                        }
-                })
-
-            }
-            binding.floatingActionButton.setOnClickListener {
-                Navigation.findNavController(it).navigate(R.id.action_listFragment_to_detailsFragment)
-            }
-        }
+        return binding.root
     }
+
+    @SuppressLint("WrongConstant")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
+
+        binding.searchButton.setOnClickListener {
+
+            binding.searchLayout.visibility = View.GONE
+            binding.errorLayout.visibility = View.GONE
+            val userInput = binding.searchText.text.toString()
+            binding.progressBarLoading.visibility = View.VISIBLE
+            viewModel.getInformation(userInput, object : CustomCallback {
+                override fun onSuccess(value: WeatherInfo) {
+                    binding.progressBarLoading.visibility = View.GONE
+                    binding.searchLayout.visibility = View.VISIBLE
+                    binding.cityNameSearchLayoutView.text = value.name
+                    binding.minTempSearchLayoutView.append(value.main.tempMin.toString())
+                    binding.maxTempSearchLayoutView.append(value.main.tempMax.toString())
+
+                    binding.moreInfoButton.setOnClickListener {
+                        Navigation.findNavController(it).navigate(
+                            ListFragmentDirections.actionListFragmentToDetailsFragment(value)
+                        )
+                    }
+                }
+
+                override fun onFailure(errorCode: Int, message: String) {
+                    binding.progressBarLoading.visibility = View.GONE
+                    binding.errorLayout.visibility = View.VISIBLE
+                    binding.errorImageView.visibility = View.VISIBLE
+                    binding.errorCodeView.append(errorCode.toString())
+                    binding.errorMessgae.append(message)
+
+                }
+            })
+
+        }
+
+    }
+}
 
 
 
